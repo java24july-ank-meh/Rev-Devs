75064b33125dad4d63203f733b7282dc11e82c2d
Kelvin Lane <kelvin.lane.95@live.com>
2017-08-18 15:51:29 -0400

    a

--- SpringApplication/src/main/java/com/revature/dao/Post.java
+++ SpringApplication/src/main/java/com/revature/dao/Post.java
@@ -2,8 +2,10 @@ package com.revature.dao;
 
 import javax.persistence.*;
 
+@Entity
 public class Post {
 	
+	@Id
 	private long post_id;
 	
 	private long location_id;
@@ -16,6 +18,61 @@ public class Post {
 	
 	public Post() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public Post(long post_id, long location_id, long user_id, long type_id, String content) {
+		super();
+		this.post_id = post_id;
+		this.location_id = location_id;
+		this.user_id = user_id;
+		this.type_id = type_id;
+		this.content = content;
+	}
+	
+	@Override
+	public String toString() {
+		return "Post [post_id=" + post_id + ", location_id=" + location_id + ", user_id=" + user_id + ", type_id="
+				+ type_id + ", content=" + content + "]";
+	}
+
+	public long getPost_id() {
+		return post_id;
+	}
+
+	public void setPost_id(long post_id) {
+		this.post_id = post_id;
+	}
+
+	public long getLocation_id() {
+		return location_id;
+	}
+
+	public void setLocation_id(long location_id) {
+		this.location_id = location_id;
+	}
+
+	public long getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(long user_id) {
+		this.user_id = user_id;
+	}
+
+	public long getType_id() {
+		return type_id;
+	}
+
+	public void setType_id(long type_id) {
+		this.type_id = type_id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
 	}
 
 }
--- SpringApplication/src/main/java/com/revature/dao/PostType.java
+++ SpringApplication/src/main/java/com/revature/dao/PostType.java
@@ -2,14 +2,43 @@ package com.revature.dao;
 
 import javax.persistence.*;
 
+@Entity
 public class PostType {
 
+	@Id
 	private long type_id;
 	
 	private String type;
 	
 	public PostType() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public PostType(long type_id, String type) {
+		super();
+		this.type_id = type_id;
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "PostType [type_id=" + type_id + ", type=" + type + "]";
+	}
+
+	public long getType_id() {
+		return type_id;
+	}
+
+	public void setType_id(long type_id) {
+		this.type_id = type_id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 }
