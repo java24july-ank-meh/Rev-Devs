50105e7e07e7f1cce954e82d503c91b587d6a4ad
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2017-08-18 21:27:06 -0400

    Moved files into appropriate place so Spring can find them

--- SpringApplication/src/main/java/com/revature/controllers/ApplicationController.java
+++ SpringApplication/src/main/java/com/revature/controllers/ApplicationController.java
@@ -1,14 +1,14 @@
 package com.revature.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ApplicationController {
 
-	@RequestMapping("/app")
+	@RequestMapping("/app/login")
 	public String home() {
-		return "index.html";
+		return "/login.html";
 	}
 
 }
