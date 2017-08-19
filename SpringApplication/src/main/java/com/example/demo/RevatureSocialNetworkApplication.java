c5880f352e0d781ca90f19672722242636deaf9a
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2017-08-18 20:44:58 -0400

    Moved login html page and create initial routes

--- SpringApplication/src/main/java/com/example/demo/RevatureSocialNetworkApplication.java
+++ SpringApplication/src/main/java/com/example/demo/RevatureSocialNetworkApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({"com.revature.controllers", "com.revature.restControllers"})
 public class RevatureSocialNetworkApplication {
 
 	public static void main(String[] args) {
