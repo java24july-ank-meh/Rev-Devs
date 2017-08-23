807c1b36e66424be60482db522503a1dca5a05c5
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2017-08-23 14:00:47 -0400

    Added locationDAO to location rest controller

--- SpringApplication/src/main/java/com/revature/application/restControllers/LocationController.java
+++ SpringApplication/src/main/java/com/revature/application/restControllers/LocationController.java
@@ -1,56 +1,52 @@
 package com.revature.application.restControllers;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.application.beans.Greeting;
-import com.revature.application.dao.Location;
+import com.revature.application.dao.LocationDao;
+import com.revature.application.dao.beans.Location;
 
 @RestController
 @RequestMapping("/locations")
 public class LocationController {
-
-	private Location location = new Location(1, "city", 12.12, 12.12);
+	
+	@Autowired
+	LocationDao locationDAO;
 	
 	/*
 	 * All GET requests
 	 */
 	@RequestMapping(path = "", method = RequestMethod.GET)
-	public Collection<Location> readAllLocations() {
-		// Get all the locations from db
-		List<Location> locations = new ArrayList<>();
-		locations.add(location);
-		return locations;
+	public Set<Location> readAllLocations() {
+		return locationDAO.readAll();
 	}
 
 	@RequestMapping(path = "/{locationId}", method = RequestMethod.GET)
 	public Location readLocationById(@PathVariable long locationId) {
-		// Get single location from db by id
-		return location;
+		return locationDAO.read(locationId);
 	}
 
 	/*
 	 * All POST requests
 	 */
 	@RequestMapping(path = "", method = RequestMethod.POST)
-	public Location createLocation(/* Take in a location object */) {
+	public boolean createLocation(/* Take in a location object */) {
 		// Add a new location to the db
-		return location;
+		return true;
 	}
 
 	/*
 	 * All DELETE requests
 	 */
 	@RequestMapping(path = "/{locationId}", method = RequestMethod.DELETE)
-	public Location deleteLocation(@PathVariable long locationId) {
+	public boolean deleteLocation(@PathVariable long locationId) {
 		// Delete a single location
-		return location;
+		return locationDAO.deleteById(locationId);
 	}
 
 }
