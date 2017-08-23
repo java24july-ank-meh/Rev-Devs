32e2c029bef5cb8b7a627f9334484a548d667a1e
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2017-08-23 15:49:02 -0400

    Added hibernate jar

--- SpringApplication/src/main/java/com/revature/application/dao/implementations/CompanyDaoImpl.java
+++ SpringApplication/src/main/java/com/revature/application/dao/implementations/CompanyDaoImpl.java
@@ -1,9 +1,56 @@
 package com.revature.application.dao.implementations;
 
-public class CompanyDaoImpl {
+import java.util.Set;
 
-	public CompanyDaoImpl() {
-		// TODO Auto-generated constructor stub
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.revature.application.dao.CompanyDao;
+import com.revature.application.dao.beans.Company;
+
+public class CompanyDaoImpl implements CompanyDao {
+
+	@Autowired
+	SessionFactory sf;
+	
+	
+	@Override
+	@Transactional
+	public boolean create(Company company) {
+		// TODO Auto-generated method stub
+		return false;
 	}
+
+	@Override
+	public Company read(long company_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Company> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(Company company) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Company company) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteById(long company_id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
 
 }
