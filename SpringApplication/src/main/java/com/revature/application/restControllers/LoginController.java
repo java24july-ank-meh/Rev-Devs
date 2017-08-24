8a6fcdbf8ee4aede8930e33a6ba30263fdbd907b
Amitai Ilan <amitai.ilan@gmail.com>
2017-08-24 01:39:11 -0400

    login controller

--- SpringApplication/src/main/java/com/revature/application/restControllers/LoginController.java
+++ SpringApplication/src/main/java/com/revature/application/restControllers/LoginController.java
@@ -1,34 +1,49 @@
 package com.revature.application.restControllers;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.application.beans.Greeting;
+import com.revature.application.dao.EmployeeDao;
+import com.revature.application.dao.beans.Employee;
 
 @RestController
 public class LoginController {
 
+	@Autowired
+	EmployeeDao employeeDAO;
+	
 	/*
 	 * Main handler for logging in a user
 	 */
 	@RequestMapping(path = "/login", method = RequestMethod.POST)
-	public Greeting login() {
+	public Greeting login(String username, String password, HttpSession session) {
 		// Search in the database
-
-		// Start a session
-
-		// Return Success or Failure
-		return new Greeting(1, "Logged In");
+		Employee user = employeeDAO.read(username);
+		if(user.getPassword().equals(password)) {
+			// Start a session
+			session.setAttribute("id", user.getEmployeeId());
+	
+			// Return Success
+			return new Greeting(1, "Logged In");
+		} else {
+			// Return Failure
+			return new Greeting(2, "Incorrect password");
+		}
 	}
 
 	/*
 	 * Main handler for logging out a user
 	 */
 	@RequestMapping(path = "/logout", method = RequestMethod.GET)
-	public Greeting logout() {
+	public Greeting logout(HttpSession session) {
 		// Destroy session
-
+		session.invalidate();
+		
 		// Return success or failure
 		return new Greeting(1, "Logged Out");
 	}
