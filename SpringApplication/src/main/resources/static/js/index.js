e9fe73cbe06f4c1216206708c8e7f1657cc5191a
Amitai Ilan <amitai.ilan@gmail.com>
2017-08-24 16:15:58 -0400

    fixed image

--- SpringApplication/src/main/resources/static/js/index.js
+++ SpringApplication/src/main/resources/static/js/index.js
@@ -2,16 +2,19 @@ let app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
 	$routeProvider
 	.when("/", {
-		templateUrl : "home.html"
+		templateUrl : "home.html",
+		activetab: 'home'
 	})
 	.when("/profile", {
-		templateUrl : "profile.html"
+		templateUrl : "profile.html",
+		activetab: 'profile'
 	})
 	.when("/explore", {
-		templateUrl : "explore.html"
+		templateUrl : "explore.html",
+		activetab: 'explore'
 	});
 });
 
 app.run(function($rootScope, $http, $location){
-	
+	$rootScope.page = 'home';
 });
