e33d3867d2d829ee9cbcc72d74691285f86a3557
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2017-08-26 03:57:27 -0400

    Added tests to company rest controller

--- SpringApplication/src/test/java/com/revature/application/restControllers/CompanyControllerTest.java
+++ SpringApplication/src/test/java/com/revature/application/restControllers/CompanyControllerTest.java
@@ -1,8 +1,7 @@
 package com.revature.application.restControllers;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.isA;
+import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,11 +14,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.nio.charset.Charset;
 import java.util.*;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -37,128 +36,120 @@ import com.revature.application.dao.beans.forms.CompanyForm;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = RevatureSocialNetworkApplication.class)
 public class CompanyControllerTest {
-
-	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
-
-	private MockMvc mockMvc;
-
-	// Some global list of comapnies
-	private List<Company> companies;
-
-	@Mock
-	private CompanyDao mockComDao;
-	@InjectMocks
-	private CompanyController companyController;
-
-	@Before
-	public void setup() throws Exception {
-
-		MockitoAnnotations.initMocks(this);
-
-		this.mockMvc = MockMvcBuilders.standaloneSetup(companyController).build();
-		// Setup all the companies info
-		companies = new ArrayList<>();
-
-		Company company = new Company(new Location(), "Bobs");
-		company.setCompanyId(1L);
-
-		companies.add(company);
-
-		company = new Company();
-		company.setCompanyId(8L);
-		company.setCompanyName("Brix Phones");
-
-		companies.add(company);
-
-//		System.out.println("Setting up Company Controller Test List");
-//		for(Company c: companies) {
-//			System.out.println(c);
-//		}
-	}
-
-	/*
-	 * Only testing for success and correct response type, responses must also be
-	 * tested for correct data response
-	 */
-
-//	@Test
-	public void returnAllCompanies() throws Exception {
-
-		// When the method calls .readAll(), mockito will not call it
-		// and return the companies list instead
-		when(mockComDao.readAll()).thenReturn(companies);
-
-		// Now call the api
-		RequestBuilder rb = get("/companies").accept(contentType);
-
-		int cId = (int) companies.get(0).getCompanyId();
-
-		// Test passes
-		mockMvc.perform(rb).andExpect(status().isOk())
-		.andExpect(content().contentType(contentType))
-		//		.andDo(print())
-		.andExpect(jsonPath("$[0].companyId", is(cId)))
-		.andExpect(jsonPath("$[0].companyName", is(companies.get(0).getCompanyName())));
-
-	}
-
-//	@Test
-	public void returnSingleCompany() throws Exception {
-
-		when(mockComDao.read(any(Long.class))).thenReturn(companies.get(0));
-
-		int cId = (int) companies.get(0).getCompanyId();
-		RequestBuilder rb = get("/companies/" + cId).accept(contentType);
-
-		mockMvc.perform(rb).andExpect(status().isOk())
-		.andExpect(content().contentType(contentType))
-//		.andDo(print())
-		.andExpect(status().is2xxSuccessful())
-		.andExpect(jsonPath("$.companyId", is(cId)))
-		.andExpect(jsonPath("$.companyName", is(companies.get(0).getCompanyName())));
-
-	}
-
-	@Test
-	public void createCompanyPass() throws Exception {
-
-	    when(mockComDao.create(any(CompanyForm.class))).thenReturn(true);
-
-		RequestBuilder rb = post("/companies").param("companyName", companies.get(1).getCompanyName());
-		//.accept(contentType);
-
-		mockMvc.perform(rb).andExpect(status().isOk())
-		.andExpect(content().contentType(contentType))
-//		.andDo(print())
-		.andExpect(jsonPath("$.message",is("Success")))
-		.andExpect(jsonPath("$.success",is(true)));
-	}
-
-	@Test
-	public void createCompanyFail() throws Exception {
-		RequestBuilder rb = post("/companies");
-		
-		mockMvc.perform(rb).andExpect(status().isOk())
-		.andExpect(content().contentType(contentType))
-		.andDo(print())
-		.andExpect(jsonPath("$.message",is("Failed to create new company")))
-		.andExpect(jsonPath("$.success",is(false)));
-	}
-
-//	@Test
-	public void deleteCompany() throws Exception {
-
-		when(mockComDao.deleteById(isA(Long.class))).thenReturn(true);
-
-		RequestBuilder rb = delete("/companies/"+companies.get(1).getCompanyId()).param("companyName", companies.get(1).getCompanyName());
-
-		mockMvc.perform(rb)
-		.andExpect(status().isOk())
-		.andExpect(content().contentType(contentType))
-//		.andDo(print())
-		.andExpect(jsonPath("$.message",is("Success")))
-		.andExpect(jsonPath("$.success",is(true)));;
-	}	
-
+    
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+    
+    private MockMvc mockMvc;
+    
+    @Mock
+    private CompanyDao mockComDao;
+    @InjectMocks
+    private CompanyController companyController;
+    
+    // Object we want to inject into companies
+    List<Company> companies;
+    Company company1;
+    Company company2;
+    
+    @Before
+    public void setup() throws Exception {
+        
+        MockitoAnnotations.initMocks(this);
+        
+        this.mockMvc = MockMvcBuilders.standaloneSetup(companyController).build();
+        
+        // Setup all the companies info
+        companies = new ArrayList<>();
+        
+        Location location = new Location("LA", 123.12, 123.12);
+        
+        company1 = new Company(location, "Bobs");
+        company1.setCompanyId(1L);
+        
+        company2 = new Company(location, "Brix Phones");
+        company2.setCompanyId(2L);
+        
+        companies.add(company1);
+        
+        companies.add(company2);
+    }
+    
+    @Test
+    public void returnAllCompanies() throws Exception {
+        
+        // When the method calls .readAll(), mockito will not call it
+        // and return the companies list instead
+        when(mockComDao.readAll()).thenReturn(companies);
+        
+        // Now call the api
+        RequestBuilder rb = get("/companies").accept(contentType);
+        
+        // Test passes
+        mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$", Matchers.hasSize(2)))
+                .andExpect(jsonPath("$[0].companyId",
+                        Matchers.is((int) companies.get(0).getCompanyId())))
+                .andExpect(jsonPath("$[0].companyName",
+                        Matchers.is(companies.get(0).getCompanyName())))
+                .andExpect(jsonPath("$[0].location", Matchers.notNullValue()))
+                .andExpect(jsonPath("$[1].companyId",
+                        Matchers.is((int) companies.get(1).getCompanyId())))
+                .andExpect(jsonPath("$[1].companyName",
+                        Matchers.is(companies.get(1).getCompanyName())))
+                .andExpect(jsonPath("$[1].location", Matchers.notNullValue()));
+        
+    }
+    
+    @Test
+    public void returnSingleCompany() throws Exception {
+        
+        when(mockComDao.read(anyLong())).thenReturn(company1);
+        
+        RequestBuilder rb = get("/companies/" + company1.getCompanyId()).accept(contentType);
+        
+        mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.companyId", Matchers.is((int) company1.getCompanyId())))
+                .andExpect(jsonPath("$.companyName", Matchers.is(company1.getCompanyName())))
+                .andExpect(jsonPath("$.location", Matchers.notNullValue()));
+        
+    }
+    
+    @Test
+    public void createCompanyPass() throws Exception {
+        
+        when(mockComDao.create(any(CompanyForm.class))).thenReturn(true);
+        
+        RequestBuilder rb = post("/companies")
+                .param("locationId", company1.getLocation().getLocationId() + "")
+                .param("companyName", company1.getCompanyName());
+        
+        mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.message", Matchers.is("Success")))
+                .andExpect(jsonPath("$.success", Matchers.is(true)));
+    }
+    
+    @Test
+    public void createCompanyFail() throws Exception {
+        
+        when(mockComDao.create(any(CompanyForm.class))).thenReturn(true);
+        
+        RequestBuilder rb = post("/companies");
+        
+        mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.success", Matchers.is(false)));
+    }
+    
+    @Test
+    public void deleteCompany() throws Exception {
+        
+        when(mockComDao.deleteById(anyLong())).thenReturn(true);
+        
+        RequestBuilder rb = delete("/companies/" + company1.getCompanyId());
+        
+        mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.message", Matchers.is("Success")))
+                .andExpect(jsonPath("$.success", Matchers.is(true)));
+    }
+    
 }
