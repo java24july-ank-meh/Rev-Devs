19fe97f90b8a7b5663b7213ba7ee6641d7cb97fb
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2017-08-26 03:58:47 -0400

    Cleaned location controller test

--- SpringApplication/src/test/java/com/revature/application/restControllers/LocationControllerTest.java
+++ SpringApplication/src/test/java/com/revature/application/restControllers/LocationControllerTest.java
@@ -2,6 +2,7 @@ package com.revature.application.restControllers;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.nio.charset.Charset;
 import java.util.*;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,147 +41,148 @@ import com.revature.application.dao.beans.forms.LocationForm;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = RevatureSocialNetworkApplication.class)
 public class LocationControllerTest {
-
-	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
-
-	private MockMvc mockMvc;
-
-	// list for readAll test
-	private List<Location> locations;
-
-	@Mock
-	private LocationDao mockLocDao;
-	@Mock
-	private HotSpotDao mockHSDao;
-	@InjectMocks
-	private LocationController locationController;
-
-	@Before
-	public void setup() throws Exception {
-
-		MockitoAnnotations.initMocks(this);
-
-		this.mockMvc = MockMvcBuilders.standaloneSetup(locationController).build();
-
-		locations = new ArrayList<Location>();
-		Location location = new Location("test1", 22.2, 111.1);
-		location.setLocationId(30L);
-		locations.add(location);
-	}
-
-	/*
-	 * Only testing for success and correct response type, responses must also be
-	 * tested for correct data response
-	 */
-
-//	@Test
-	public void returnAllLocations() throws Exception {
-		// for method call it returns our created list
-		// System.out.println("locations");
-		// System.out.println(locations);
-
-		when(mockLocDao.readAll()).thenReturn(locations);
-
-		// System.out.println("locations2");
-		// System.out.println(listMock.readAll());
-
-		mockMvc.perform(get("/locations")).andExpect(status().isOk()).andExpect(content().contentType(contentType))
-		// .andDo(print())
-		.andExpect(jsonPath("$[0].locationId", is((int) locations.get(0).getLocationId())))
-		.andExpect(jsonPath("$[0].longitude", is(locations.get(0).getLongitude())))
-		.andExpect(jsonPath("$[0].lattitude", is(locations.get(0).getLattitude())))
-		.andExpect(jsonPath("$[0].city", is(locations.get(0).getCity())));
-
-	}
-
-//	@Test
-	public void returnSingleLocation() throws Exception {
-
-		when(mockLocDao.read(30)).thenReturn(locations.get(0));
-
-		int lId = (int) locations.get(0).getLocationId();
-
-		mockMvc.perform(get("/locations/" + lId)).andExpect(status().isOk())
-		.andExpect(content().contentType(contentType))
-		// .andDo(print())
-		.andExpect(jsonPath("$.locationId", is(lId)))
-		.andExpect(jsonPath("$.longitude", is(locations.get(0).getLongitude())))
-		.andExpect(jsonPath("$.lattitude", is(locations.get(0).getLattitude())))
-		.andExpect(jsonPath("$.city", is(locations.get(0).getCity())));
-	}
-
-//	@Test
-	public void createLocationPass() throws Exception {
-		
-	    when(mockLocDao.create(any(LocationForm.class))).thenReturn(true);
-
-		RequestBuilder rb = post("/locations").param("city", locations.get(0).getCity())
-				.param("lattitude", locations.get(0).getLattitude() + "")
-				.param("longitude", locations.get(0).getLongitude() + "");
-
-		mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
-		// .andDo(print())
-		.andExpect(jsonPath("$.message", is("Success"))).andExpect(jsonPath("$.success", is(true)));
-	}
-
-//	@Test
-	public void createLocationFail() throws Exception {
-//		when(mockLocDao.create(isA(Location.class))).thenReturn(true);
-
-		RequestBuilder rb = post("/locations");
-
-		mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
-		// .andDo(print())
-		.andExpect(jsonPath("$.message", is("Failed to create new location")))
-		.andExpect(jsonPath("$.success", is(false)));
-	}
-
-//	@Test
-	public void createHotSpotPass() throws Exception {
-	    
-		when(mockHSDao.createHotSpot(any(HotSpotForm.class))).thenReturn(true);
-
-		int lId = (int)locations.get(0).getLocationId() ;
-		RequestBuilder rb = post("/locations/"+lId+"/hotspots")
-				.param("locationId", lId+"")
-				.param("lattitude", locations.get(0).getLattitude()+"")
-				.param("longitude", locations.get(0).getLongitude()+"");
-
-		mockMvc.perform(rb).andExpect(status().isOk())
-		.andExpect(content().contentType(contentType))
-		//		.andDo(print())
-		.andExpect(jsonPath("$.message",is("Success")))
-		.andExpect(jsonPath("$.success",is(true)));
-	}
-
-	@Test
-	public void createHotSpotFail() throws Exception {
-//		when(mockHSDao.createHotSpot(isA(HotSpot.class))).thenReturn(true);
-
-		int lId = (int) locations.get(0).getLocationId();
-		RequestBuilder rb = post("/locations/" + lId + "/hotspots");
-
-		mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
-		 .andDo(print())
-		.andExpect(jsonPath("$.message", is("Failed to create new hot spot")))
-		.andExpect(jsonPath("$.success", is(false)));
-	}
-
-//	@Test
-	public void deleteLocation() throws Exception {
-		mockMvc.perform(delete("/locations/1")).andExpect(status().isOk())
-		.andExpect(content().contentType(contentType));
-
-		when(mockLocDao.deleteById(isA(Long.class))).thenReturn(true);
-
-		RequestBuilder rb = delete("/locations/" + locations.get(0).getLocationId());// .param("city",
-		// locations.get(0).getCity());
-
-		mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
-		// .andDo(print())
-		.andExpect(jsonPath("$.message", is("Success"))).andExpect(jsonPath("$.success", is(true)));
-		;
-	}
-
+    
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+    
+    private MockMvc mockMvc;
+    
+    @Mock
+    private LocationDao mockLocDao;
+    @Mock
+    private HotSpotDao mockHSDao;
+    @InjectMocks
+    private LocationController locationController;
+    
+    // list for readAll test
+    private List<Location> locations;
+    private Location location1;
+    private Location location2;
+    
+    @Before
+    public void setup() throws Exception {
+        
+        MockitoAnnotations.initMocks(this);
+        
+        this.mockMvc = MockMvcBuilders.standaloneSetup(locationController).build();
+        
+        location1 = new Location("test1", 123.12, 123.12);
+        location1.setLocationId(30L);
+        location2 = new Location("test2", 123.12, 123.12);
+        location2.setLocationId(31L);
+        
+        locations = new ArrayList<Location>();
+        
+        locations.add(location1);
+        locations.add(location2);
+    }
+    
+    @Test
+    public void returnAllLocations() throws Exception {
+        
+        when(mockLocDao.readAll()).thenReturn(locations);
+        
+        mockMvc.perform(get("/locations")).andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$", Matchers.hasSize(2)))
+                .andExpect(jsonPath("$[0].locationId",
+                        Matchers.is((int) locations.get(0).getLocationId())))
+                .andExpect(jsonPath("$[0].longitude", Matchers.is(locations.get(0).getLongitude())))
+                .andExpect(jsonPath("$[0].lattitude", Matchers.is(locations.get(0).getLattitude())))
+                .andExpect(jsonPath("$[0].city", Matchers.is(locations.get(0).getCity())))
+                .andExpect(jsonPath("$[0].employees", Matchers.notNullValue()))
+                .andExpect(jsonPath("$[0].companies", Matchers.notNullValue()))
+                .andExpect(jsonPath("$[0].posts", Matchers.notNullValue()))
+                .andExpect(jsonPath("$[1].locationId",
+                        Matchers.is((int) locations.get(1).getLocationId())))
+                .andExpect(jsonPath("$[1].longitude", Matchers.is(locations.get(1).getLongitude())))
+                .andExpect(jsonPath("$[1].lattitude", Matchers.is(locations.get(1).getLattitude())))
+                .andExpect(jsonPath("$[1].city", Matchers.is(locations.get(1).getCity())))
+                .andExpect(jsonPath("$[1].employees", Matchers.notNullValue()))
+                .andExpect(jsonPath("$[1].companies", Matchers.notNullValue()))
+                .andExpect(jsonPath("$[1].posts", Matchers.notNullValue()));
+        
+    }
+    
+    @Test
+    public void returnSingleLocation() throws Exception {
+        
+        when(mockLocDao.read(location1.getLocationId())).thenReturn(location1);
+        
+        mockMvc.perform(get("/locations/" + location1.getLocationId())).andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.locationId",
+                        Matchers.is((int) locations.get(0).getLocationId())))
+                .andExpect(jsonPath("$.longitude", Matchers.is(locations.get(0).getLongitude())))
+                .andExpect(jsonPath("$.lattitude", Matchers.is(locations.get(0).getLattitude())))
+                .andExpect(jsonPath("$.city", Matchers.is(locations.get(0).getCity())))
+                .andExpect(jsonPath("$.employees", Matchers.notNullValue()))
+                .andExpect(jsonPath("$.companies", Matchers.notNullValue()))
+                .andExpect(jsonPath("$.posts", Matchers.notNullValue()));
+    }
+    
+    @Test
+    public void createLocationPass() throws Exception {
+        
+        when(mockLocDao.create(any(LocationForm.class))).thenReturn(true);
+        
+        RequestBuilder rb = post("/locations").param("city", location1.getCity())
+                .param("lattitude", location1.getLattitude() + "")
+                .param("longitude", location1.getLongitude() + "");
+        
+        mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.message", is("Success")))
+                .andExpect(jsonPath("$.success", is(true)));
+    }
+    
+    @Test
+    public void createLocationFail() throws Exception {
+        
+        when(mockLocDao.create(any(LocationForm.class))).thenReturn(true);
+        
+        RequestBuilder rb = post("/locations");
+        
+        mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.success", is(false)));
+    }
+    
+    @Test
+    public void createHotSpotPass() throws Exception {
+        
+        when(mockHSDao.createHotSpot(any(HotSpotForm.class))).thenReturn(true);
+        
+        RequestBuilder rb = post("/locations/" + location1.getLocationId() + "/hotspots")
+                .param("locationId", location1.getLocationId() + "")
+                .param("lattitude", location1.getLattitude() + "")
+                .param("longitude", location1.getLongitude() + "");
+        
+        mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.message", is("Success")))
+                .andExpect(jsonPath("$.success", is(true)));
+    }
+    
+    @Test
+    public void createHotSpotFail() throws Exception {
+        
+        when(mockHSDao.createHotSpot(any(HotSpotForm.class))).thenReturn(true);
+        
+        RequestBuilder rb = post("/locations/" + location1.getLocationId() + "/hotspots");
+        
+        mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.success", Matchers.is(false)));
+    }
+    
+    @Test
+    public void deleteLocation() throws Exception {
+        
+        when(mockLocDao.deleteById(anyLong())).thenReturn(true);
+        
+        RequestBuilder rb = delete("/locations/" + location1.getLocationId());
+        
+        mockMvc.perform(rb).andExpect(status().isOk()).andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.message", Matchers.is("Success")))
+                .andExpect(jsonPath("$.success", Matchers.is(true)));
+        ;
+    }
+    
 }
