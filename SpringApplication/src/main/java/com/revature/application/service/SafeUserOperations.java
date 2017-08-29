48d4339b0e4fade3210b57f7713c2bd70b081b5b
Kelvin Lane <kelvin.lane.95@live.com>
2017-08-29 18:28:16 -0400

    Added to interface

--- SpringApplication/src/main/java/com/revature/application/service/SafeUserOperations.java
+++ SpringApplication/src/main/java/com/revature/application/service/SafeUserOperations.java
@@ -1,11 +1,15 @@
 package com.revature.application.service;
 
+import javax.servlet.http.HttpSession;
+
 import com.revature.application.dao.beans.Employee;
 
 public interface SafeUserOperations {
 
 	public void authenticateById(String id);
 	public Employee loadEmployee();
-//	public void saveEmployee(Employee e);//cant do without httpsession
+	public void saveEmployee(Employee e, HttpSession s);//cant do without httpsession
+	public void setSession(HttpSession s);
+	public boolean isSessionValid(HttpSession s);
 	
 }
