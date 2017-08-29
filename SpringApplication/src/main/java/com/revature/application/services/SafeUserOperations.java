71de818be40a34c0b3d40e7840b56ecbdf3ff73c
Kelvin Lane <kelvin.lane.95@live.com>
2017-08-29 18:55:46 -0400

    changed path and revamped interface

---
+++ SpringApplication/src/main/java/com/revature/application/services/SafeUserOperations.java
@@ -0,0 +1,24 @@
+package com.revature.application.services;
+
+import javax.servlet.http.HttpSession;
+
+import com.revature.application.dao.beans.Employee;
+
+public interface SafeUserOperations {
+
+	//checks login  credentials
+	public boolean checkLoginCredits(String un, String pw);
+	
+	//authenticating user
+	public HttpSession authenticateById(Long id);
+	
+	//if user needs to be loaded from master session
+	public Employee loadEmployee();
+	
+	//checks if authenticated or not and have employee id
+	public boolean isSessionValid();
+	
+	//update user taken from out of controller
+	public void updateUser(Employee e2);
+	
+}
