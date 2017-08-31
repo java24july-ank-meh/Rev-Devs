399149a69b272b53ff609fdf4bab88c0aedb66da
Amitai Ilan <amitai.ilan@gmail.com>
2017-08-31 13:12:31 -0400

    send location from the location page to locationMap page

--- SpringApplication/src/main/resources/static/js/locationMapController.js
+++ SpringApplication/src/main/resources/static/js/locationMapController.js
@@ -1,6 +1,15 @@
-app.controller('locationMapController', function($rootScope, $scope,$http, $routeParams) {
-		function example(){
-			console.log($routeParams.loc);
-		}
-		example();
-		});
+app.controller('locationMapController', function($rootScope, $scope,$http, $routeParams, $window) {
+		$scope.init = function(){
+			//$window.locationId = $routeParams.loc;
+			
+			$http({
+				method: 'GET',
+				url: '/locations/'+$routeParams.loc,
+			}).then(function successCallback(response){
+				$window.locationLat = response.data.lattitude;
+				$window.locationLng = response.data.longitude;
+				
+			}, function errorCallback(response){
+			});
+		};
+});
